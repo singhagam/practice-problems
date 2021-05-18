918cb7a03cd558d89e9fde74d6c5d8a68d30912b
singhagam <agamsingh837@gmail.com>
2021-05-18 22:58:37 +0530

    Initial commit

---
+++ src/main/java/com/practice/TwoSum.java
@@ -0,0 +1,33 @@
+package com.practice;
+
+import java.util.Arrays;
+import java.util.HashSet;
+import java.util.stream.IntStream;
+
+public class TwoSum {
+    public static void main(String[] args) {
+        int[] arr={3,2,4};
+        int target=6;
+        int result[]=twosum(arr,target);
+        System.out.println(result[0]+" "+result[1]);
+    }
+
+    private static int[] twosum(int[] nums, int target) {
+        HashSet<Integer> set= new HashSet<>();
+        int res[]= new int[2];
+        for(int i=0;i<nums.length;i++)
+        {
+            int temp= target-nums[i];
+            if(set.contains(temp))
+            {
+                res[0]=i;
+                res[1]= IntStream.range(0,nums.length-1)
+                        .filter(x -> temp == nums[x])
+                        .findFirst().orElse(-1);
+            }
+            set.add(nums[i]);
+        }
+        Arrays.sort(res);
+        return res;
+    }
+}
