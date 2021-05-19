6f932108d485baa76bed3f654279187563de1307
singhagam <agamsingh837@gmail.com>
2021-05-19 09:38:24 +0530

    added question

--- src/main/java/com/practice/TwoSum.java
+++ src/main/java/com/practice/TwoSum.java
@@ -4,6 +4,18 @@ import java.util.Arrays;
 import java.util.HashSet;
 import java.util.stream.IntStream;
 
+/**
+ * Given an array of integers nums and an integer target, return indices of the two numbers such that they add up to target.
+ *
+ * You may assume that each input would have exactly one solution, and you may not use the same element twice.
+ *
+ * Input: nums = [2,7,11,15], target = 9
+ * Output: [0,1]
+ * Output: Because nums[0] + nums[1] == 9, we return [0, 1].
+ *
+ * @author agamsingh
+ * 19-05-2021
+ */
 public class TwoSum {
     public static void main(String[] args) {
         int[] arr={3,2,4};
